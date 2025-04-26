b9ed54d3e6383b9ebf8c9d51ba33f74231b4dbba
ankhod <ankhod7@gmail.com>
2025-04-26 21:32:36 +0300

    Update ImageModal.tsx

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,26 +1,37 @@
-import Modal from 'react-modal';
-import styles from './ImageModal.module.css';
+import { useEffect, MouseEvent, KeyboardEvent } from "react";
+import "./ImageModal.module.css";
 
-Modal.setAppElement('#root');
-
-export default function ImageModal({ image, onClose }) {
-  return (
-    <Modal
-      isOpen={!!image}
-      onRequestClose={onClose}
-      className={styles.modal}
-      overlayClassName={styles.overlay}
-    >
-      <img
-        src={image.urls.regular}
-        alt={image.alt_description || 'Image'}
-        className={styles.image}
-      />
-      <div className={styles.info}>
-        <p>Author: {image.user.name}</p>
-        <p>Likes: {image.likes}</p>
-        {image.description && <p>Description: {image.description}</p>}
-      </div>
-    </Modal>
-  );
+interface ImageModalProps {
+  largeImageURL: string;
+  onClose: () => void;
 }
+
+const ImageModal: React.FC<ImageModalProps> = ({ largeImageURL, onClose }) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent): void => {
+      if (e.code === "Escape") {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown as any);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown as any);
+    };
+  }, [onClose]);
+
+  const handleBackdropClick = (e: MouseEvent<HTMLDivElement>): void => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <div className="overlay" onClick={handleBackdropClick}>
+      <div className="modal">
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ImageModal;
